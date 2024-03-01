673b8df14cb5233643257c4e1a398650fec0a1ae
xJustFox <dodydaversa@gmail.com>
2024-03-01 14:04:54 +0100

    [Upgrade] Completed MILESTONE 2

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::with('type','technologies')->paginate(5);
 
         return response()->json([
             'succes' => true,
